928bfa7ce76284e7b4660ec4705923cf670bfad4
Ganesh Kedari <ganeshkedari@live.com>
2019-07-13 17:14:02 +0530

    Crafting a Health Potion

--- Project/Crafting_a_Health_Potion.py
+++ Project/Crafting_a_Health_Potion.py
@@ -1,9 +1,13 @@
 # Import Modules
 import random
 
+# Set difficulty
+
+difficulty = 3
+
 # Set Health
 health = 50
-potion = random.randint(25, 50)
+potion = int(random.randint(25, 50) / difficulty)  # Cast to integer as division return float
 
 # Add potion
 health += potion
